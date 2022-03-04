732e6961af9c0488f67385fd321c144225db6dfb
Kunibal <alexinyamagata@gmail.com>
2022-03-04 12:20:29 +0000

    add authentication for admin and customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,8 @@
 class CustomersController < ApplicationController
+  before_action :authenticate_admin!, only: [:index]
+  before_action :ensure_correct_user, only: [:show, :edit]
+  before_action :set_customer, only: [:show, :edit]
+
   def index
   end
 
@@ -6,5 +10,26 @@ class CustomersController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def customer_params
+    params.requiere(:customer).permit(:last_name, :last_name_kana, :first_name, :first_name_kana,
+                                      :postal_code, :prefecture_code, :city, :block, :building_name)
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  # current customerとadminに許可与える
+  def ensure_correct_user
+    @customer = Customer.find(params[:id])
+    @admin = Admin.find(params[:id])
+    if @customer == current_customer || @admin == current_admin
+    else
+      redirect_to root_path(current_customer)
+    end
   end
 end
