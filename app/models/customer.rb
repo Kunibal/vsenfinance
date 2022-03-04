f6a76bd6c784342311f9f6868198e80e730d21e9
Kunibal <alexinyamagata@gmail.com>
2022-03-04 12:49:44 +0000

    fix prefecture output in show page

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,4 +12,15 @@ class Customer < ApplicationRecord
   validates :prefecture_code, presence: true
   validates :city, presence: true
   validates :block, presence: true
+
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 end
