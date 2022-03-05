272e6443b99b61371f747c4a30d1d4d9dbc2c7b3
Kunibal <alexinyamagata@gmail.com>
2022-03-05 06:00:33 +0000

    add new items action and update item new/show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,42 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_admin!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
+
+  def add_tax
+    (self.price * 1.10).round
+  end
 end
