780947bd269ae89eb4ebb0429f9cdb14b4835885
Kunibal <alexinyamagata@gmail.com>
2022-03-05 04:41:25 +0000

    update edit customer functionality

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,10 @@
 class CustomersController < ApplicationController
   before_action :authenticate_admin!, only: [:index]
   before_action :ensure_correct_user, only: [:show, :edit]
-  before_action :set_customer, only: [:show, :edit]
+  before_action :set_customer, only: [:show, :edit, :update]
 
   def index
+    @customer = Customer.all
   end
 
   def show
@@ -12,10 +13,19 @@ class CustomersController < ApplicationController
   def edit
   end
 
+  def update
+    if @customer.update(customer_params)
+      flash[:success] = "You have edited user data successfully."
+      redirect_to customer_path(@customer)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def customer_params
-    params.requiere(:customer).permit(:last_name, :last_name_kana, :first_name, :first_name_kana,
+    params.require(:customer).permit(:last_name, :last_name_kana, :first_name, :first_name_kana,
                                       :postal_code, :prefecture_code, :city, :block, :building_name)
   end
 
