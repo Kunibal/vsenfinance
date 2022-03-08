4905fddfa863a05944f42170807b295c32bd87f5
Kunibal <alexinyamagata@gmail.com>
2022-03-08 10:49:56 +0000

    update post edit with update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,30 @@
 class PostsController < ApplicationController
   before_action :authenticate_admin!, only: [:edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :body)
