9213cc5747623dc03dd73b20ed762670df691f92
Kunibal <alexinyamagata@gmail.com>
2022-03-29 05:07:13 +0000

    fix admin dashboard graph weekly revenue display

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -18,13 +18,13 @@ class HomesController < ApplicationController
     @post = Post.new
 
     # ECサイトの情報をグラフ化（今週分）
-    end_date = Date.today.beginning_of_week + 6.days
+    end_date = Date.today.end_of_week
     start_date = Date.today.beginning_of_week
     zero_filled_date_range = (start_date.to_date..end_date.to_date).map{ |date| [date, 0] }.to_h
     @ec_chart = zero_filled_date_range.merge(Order.where(created_at: start_date..end_date).group_by_day(:created_at).sum(:order_price))
 
     # 店内の情報をグラフ化（今週分）
-    end_date = Date.today.beginning_of_week + 6.days
+    end_date = Date.today.end_of_week
     start_date = Date.today.beginning_of_week
     zero_filled_date_range = (start_date.to_date..end_date.to_date).map{ |date| [date, 0] }.to_h
     @store_chart = zero_filled_date_range.merge(Revenue.where(created_at: start_date..end_date).group_by_day(:created_at).sum(:amount))
