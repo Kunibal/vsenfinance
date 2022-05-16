5faeb0252d0741d84d5dcbb5354656c390bb4d8c
Kunibal <alexinyamagata@gmail.com>
2022-05-16 04:23:14 +0000

    apply styling to indicate copy status at login-page

--- app/javascript/packs/copy_to_clipboard.js
+++ app/javascript/packs/copy_to_clipboard.js
@@ -1,28 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  const mailToCopy = document.getElementById("mailToCopy");
+  // Add click event listener for clicking the text
+  var styleSheetWrapperEl = document.getElementById('login-page');
+  styleSheetWrapperEl.addEventListener('click', function (e) {
+    if (e.target.className !== 'textToCopy') return;
 
-  // var element = document.querySelector('td');
-  // console.log(element.closest('div'));
+    var element = e.target;
+    flashElement(element);
+    copyText(element);
+  });
 
-    mailToCopy.addEventListener("click", () => {
-      const tempElement = document.createElement("textarea");
-      tempElement.value = mailToCopy.innerHTML;
+    function copyText(element) {
+      var textToCopy = element.innerText;
+      var tempElement = document.createElement("textarea");
+      tempElement.value = textToCopy;
       document.body.appendChild(tempElement);
       tempElement.select();
       document.execCommand("Copy");
       document.body.removeChild(tempElement);
-    });
+    }
 
-  const passwordToCopy = document.getElementById("passwordToCopy");
-
-    passwordToCopy.addEventListener("click", () => {
-      const tempElement = document.createElement("textarea");
-      tempElement.value = passwordToCopy.innerHTML;
-      document.body.appendChild(tempElement);
-      tempElement.select();
-      document.execCommand("Copy");
-      document.body.removeChild(tempElement);
-    });
+    function flashElement(element) {
+      element.classList.add("flash");
+      document.addEventListener("transitionend", function() {
+        setTimeout(function() {
+          element.classList.remove("flash");
+        }, 1000);
+      });
+    }
 
 });
